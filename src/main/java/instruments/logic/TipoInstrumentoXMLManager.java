0f2d945ed66eab9e3469d1059ba62588915943b4
Key Cortés <89230619+AlphaK03@users.noreply.github.com>
2023-09-09 02:25:29 -0600

    We have added the functionality to save and load `Instrumento` data using XML files. #6

--- src/main/java/instruments/logic/TipoInstrumentoXMLManager.java
+++ src/main/java/instruments/logic/TipoInstrumentoXMLManager.java
@@ -91,4 +91,116 @@ public class TipoInstrumentoXMLManager {
 
         return tipos;
     }
+    public static void guardarInstrumentos(List<Instrumento> instrumentos, String filePath) {
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+            Document doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement("instrumentos");
+            doc.appendChild(rootElement);
+
+            for (Instrumento instrumento : instrumentos) {
+                Element instrumentoElement = doc.createElement("instrumento");
+                rootElement.appendChild(instrumentoElement);
+
+                // Serie
+                Element serieElement = doc.createElement("serie");
+                serieElement.appendChild(doc.createTextNode(instrumento.getSerie()));
+                instrumentoElement.appendChild(serieElement);
+
+                // Descripcion
+                Element descripcionElement = doc.createElement("descripcion");
+                descripcionElement.appendChild(doc.createTextNode(instrumento.getDescripcion()));
+                instrumentoElement.appendChild(descripcionElement);
+
+                // Tolerancia
+                Element toleranciaElement = doc.createElement("tolerancia");
+                toleranciaElement.appendChild(doc.createTextNode(String.valueOf(instrumento.getTolerancia())));
+                instrumentoElement.appendChild(toleranciaElement);
+
+                // Maximo
+                Element maximoElement = doc.createElement("maximo");
+                maximoElement.appendChild(doc.createTextNode(String.valueOf(instrumento.getMaximo())));
+                instrumentoElement.appendChild(maximoElement);
+
+                // Minimo
+                Element minimoElement = doc.createElement("minimo");
+                minimoElement.appendChild(doc.createTextNode(String.valueOf(instrumento.getMinimo())));
+                instrumentoElement.appendChild(minimoElement);
+
+                // TipoInstrumento (asumiendo que el tipo se guarda como código)
+                Element tipoInstrumentoElement = doc.createElement("tipoInstrumento");
+                tipoInstrumentoElement.appendChild(doc.createTextNode(instrumento.getTipoInstrumento().getCodigo()));
+                instrumentoElement.appendChild(tipoInstrumentoElement);
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(filePath));
+
+            transformer.transform(source, result);
+
+        } catch (ParserConfigurationException | TransformerException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static List<Instrumento> cargarInstrumentos(String filePath) {
+        List<Instrumento> instrumentos = new ArrayList<>();
+
+        try {
+            File file = new File(filePath);
+            if (file.exists()) {
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(file);
+
+                NodeList instrumentoNodes = doc.getElementsByTagName("instrumento");
+
+                for (int i = 0; i < instrumentoNodes.getLength(); i++) {
+                    Node instrumentoNode = instrumentoNodes.item(i);
+
+                    if (instrumentoNode.getNodeType() == Node.ELEMENT_NODE) {
+                        Element instrumentoElement = (Element) instrumentoNode;
+
+                        // Obtén los valores dentro del bucle
+                        String serie = instrumentoElement.getElementsByTagName("serie").item(0).getTextContent();
+                        String descripcion = instrumentoElement.getElementsByTagName("descripcion").item(0).getTextContent();
+                        double tolerancia = Double.parseDouble(instrumentoElement.getElementsByTagName("tolerancia").item(0).getTextContent());
+                        double maximo = Double.parseDouble(instrumentoElement.getElementsByTagName("maximo").item(0).getTextContent());
+                        double minimo = Double.parseDouble(instrumentoElement.getElementsByTagName("minimo").item(0).getTextContent());
+                        String tipoInstrumentoCodigo = instrumentoElement.getElementsByTagName("tipoInstrumento").item(0).getTextContent();
+
+                        // En este punto, debes buscar el TipoInstrumento correspondiente en tu lista de TipoInstrumento cargada previamente
+                        TipoInstrumento tipoInstrumento = obtenerTipoInstrumentoPorCodigo(tipoInstrumentoCodigo, "files/TiposInstrumentos.xml");
+
+                        if (tipoInstrumento != null) {
+                            Instrumento instrumento = new Instrumento(serie, descripcion, tolerancia, maximo, minimo, tipoInstrumento);
+                            instrumentos.add(instrumento);
+                        }
+                    }
+                }
+            }
+        } catch (ParserConfigurationException | SAXException | IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return instrumentos;
+    }
+
+
+
+    // Función para obtener un TipoInstrumento por su código
+    private static TipoInstrumento obtenerTipoInstrumentoPorCodigo(String codigo, String filePath) {
+        List<TipoInstrumento> tipos = cargarTiposInstrumento(filePath);
+        return tipos.stream()
+                .filter(t -> t.getCodigo().equals(codigo))
+                .findFirst()
+                .orElse(null);
+    }
+
 }
