288c98b913dc39a25fdf7e9d3798602a82f8cb91
Key Cortés <89230619+AlphaK03@users.noreply.github.com>
2023-09-30 21:23:53 -0600

    Fixing issues. #8

--- src/main/java/instruments/presentation/acercaDe/AcercaDe.java
+++ src/main/java/instruments/presentation/acercaDe/AcercaDe.java
@@ -8,15 +8,11 @@ public class AcercaDe {
     private JTextArea textArea1;
     private JTextField textField1;
     private JTextField KEYLORJOSUÉCORTÉSCASCANTETextField;
-    private JTextField PABLOROJASVILLALOBOSTextField;
-    private JTextField ANDRELRAMIREZSOLISTextField;
 
     public AcercaDe() {
         textField1.setText(" JOSE SANCHEZ SALAZAR");
         textField1.setEnabled(false);
         KEYLORJOSUÉCORTÉSCASCANTETextField.setEnabled(false);
-        PABLOROJASVILLALOBOSTextField.setEnabled(false);
-        ANDRELRAMIREZSOLISTextField.setEnabled(false);
     }
 
     public JPanel getAcercaDePanel() {
